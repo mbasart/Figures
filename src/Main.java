3405160a3bc83f7a48a45578b1b45fbb08edabfa
mbasart <mbd2797@gmail.com>
2018-09-21 10:15:32 +0200

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        //I create a rectangle class
+        Rectangle rectangle1 = new Rectangle(3,5);
+
+        //print out the area
+        System.out.println(rectangle1.area);
+    }
+}
